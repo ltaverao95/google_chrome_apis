bb19c0b33c190f3852f2b96295fcccca95dd2ef6
luistaveraflyr <luis.tavera@newshore.es>
2025-09-22 08:07:29 -0500

    Add SpeedInsights component to main application for performance monitoring

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './styles/global.css';
-import './styles/theme.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import App from "./App";
+import "./styles/global.css";
+import "./styles/theme.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
+      <SpeedInsights />
       <App />
     </BrowserRouter>
   </React.StrictMode>
